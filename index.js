f481f697e4a3feee015d31e64e87a246d761ded6
Benson Mwangi <kimaniben@gmail.com>
2023-10-26 23:02:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((acc, side) => acc + side, 0);
+    }
+  }
+  
+  class Triangle extends Polygon {
+    get isValid() {
+      if (this.countSides !== 3) return false;
+      let [a, b, c] = this.sides;
+      return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+  }
+  
+  class Square extends Polygon {
+    get isValid() {
+      if (this.countSides !== 4) return false;
+      return this.sides.every(side => side === this.sides[0]);
+    }
+  
+    get area() {
+      if (!this.isValid) return;
+      return this.sides[0] ** 2;
+    }
+  }
+  
+  // Testing
+  const polygon = new Polygon([3, 4, 5]);
+  console.log(polygon.countSides); // 3
+  console.log(polygon.perimeter); // 12
+  
+  const triangle = new Triangle([3, 4, 5]);
+  console.log(triangle.isValid); // true
+  
+  const square = new Square([2, 2, 2, 2]);
+  console.log(square.isValid);  // true
+  console.log(square.area);     // 4
